e1ce496fb62461d1244da0401da1990ce99d76f2
WabaScript <rjwahba@gmail.com>
2020-07-23 01:17:14 -0400

    buggy

--- server.js
+++ server.js
@@ -42,7 +42,9 @@ io.on('connection', socket => {
     //Listen for new chatMessage
     socket.on('chatMessage', msg => {
         const user = getCurrentUser(socket.id)
-        io.to(user.room).emit('message', formatMessage(user.username, msg));
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(user.username, msg));
+        }
     });
 
     //Broadcast when client disconnects - to all
